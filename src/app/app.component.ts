ffe6e4293089d19a7e33ff8ec1db04276fc792a5
santoshbussa <107455911+santoshbussa@users.noreply.github.com>
2022-06-22 18:53:18 +0530

    pus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Countries Informations Applications';
+  title = 'Countries Informations Application';
 }
